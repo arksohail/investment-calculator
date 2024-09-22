40138919bca0f2ffa9ffad7be874c6626cd8107d
arksohail <arksohail@gmail.com>
2024-09-22 20:33:20 +0530

    Setting Initial Values for the User Input Form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -9,10 +9,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  initialInvestment?: string;
-  annualInvestment?: string;
+  initialInvestment?: string = "0";
+  annualInvestment?: string = "0";
   expectedReturn?: string = "5";
-  duration?: string;
+  duration?: string = "10";
   onSubmit() {
     console.log("SUBMITTED");
     console.log("FORM VALUES", this.annualInvestment, this.duration, this.expectedReturn, this.initialInvestment);
